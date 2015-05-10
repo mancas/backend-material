fec600c3d24894ffe7cd67f6448d70c835aab5b3
Manuel <manuel.casasbarrado@gmail.com>
2015-05-10 16:54:53 +0200

    first approach js chat

--- src/MaterialDesign/BackendBundle/Controller/BackendController.php
+++ src/MaterialDesign/BackendBundle/Controller/BackendController.php
@@ -2,10 +2,22 @@
 
 namespace MaterialDesign\BackendBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class BackendController extends CustomController
 {
     public function indexAction()
     {
         return $this->render('BackendBundle:Backend:index.html.twig');
+    }
+
+    public function saveMessageAction(Request $request) {
+        $jsonResponse = json_encode(array('ok' => false));
+        ldd($request->isXmlHttpRequest(), $request->isMethod('post'));
+        if ($request->isXmlHttpRequest()) {
+           ldd($request->request);
+        }
+
+        return $this->getHttpJsonResponse($jsonResponse);
     }
 }
