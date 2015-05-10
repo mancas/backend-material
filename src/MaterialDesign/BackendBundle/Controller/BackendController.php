10a4da74ba5b54dd5dd9215edeb64bebb6acd843
Manuel <manuel.casasbarrado@gmail.com>
2015-05-10 21:01:48 +0200

    alerts

--- src/MaterialDesign/BackendBundle/Controller/BackendController.php
+++ src/MaterialDesign/BackendBundle/Controller/BackendController.php
@@ -13,9 +13,12 @@ class BackendController extends CustomController
 
     public function saveMessageAction(Request $request) {
         $jsonResponse = json_encode(array('ok' => false));
-        ldd($request->isXmlHttpRequest(), $request->isMethod('post'));
         if ($request->isXmlHttpRequest()) {
-           ldd($request->request);
+            // TODO: Get real avatar path
+            $time = new \DateTime('now');
+            $response = array('ok' => true, 'avatar' => '/material/web/bundles/backend/img/user-2.jpg',
+                'time' => $time->format('d/m/Y - H:i'));
+            $jsonResponse = json_encode($response);
         }
 
         return $this->getHttpJsonResponse($jsonResponse);
